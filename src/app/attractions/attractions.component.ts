f4c297f542338dbeb1c78a4c65ab8f5e10841d1a
Thor Andresen <thorg3@gmail.com>
2022-03-22 20:50:58 +0100

    added animations for attractions

--- src/app/attractions/attractions.component.ts
+++ src/app/attractions/attractions.component.ts
@@ -37,8 +37,6 @@ export class AttractionsComponent implements OnInit {
       return;
     }
 
-
-
     slider.addEventListener('touchstart', e => {
       touchstartX = e.changedTouches[0].screenX
     })
@@ -54,14 +52,14 @@ export class AttractionsComponent implements OnInit {
     var activeIndicatorID = activeIndicator?.id as string;
 
     if (touchendX < touchstartX && (touchstartX - touchendX) > 50) {
-      this.changeSlide(parseInt(activeIndicatorID) + 1);
+      this.changeSlide(parseInt(activeIndicatorID) + 1, "left");
     }
     if (touchendX > touchstartX && (touchendX - touchstartX) > 50) {
-      this.changeSlide(parseInt(activeIndicatorID) - 1);
+      this.changeSlide(parseInt(activeIndicatorID) - 1, "right");
     }
   }
 
-  changeSlide(slide: number): void {
+  changeSlide(slide: number, direction: string): void {
     var cards = this.document.getElementsByClassName('attraction-card');
     var indicators = this.document.getElementsByClassName('attraction-indicator');
     var activeIndicator = this.document.querySelector('.attraction-indicator.active');
@@ -78,9 +76,23 @@ export class AttractionsComponent implements OnInit {
     // Set active on new active indicator and remove hidden from active card.
     indicators[slide].classList.toggle('active');
     cards[slide].classList.toggle('hidden');
+
+    if (direction == "click") {
+      cards[slide].classList.toggle('w3-animate-opacity');
+
+      // TODO: Move this to init and add eventlistener on all cards.
+      cards[slide].addEventListener('animationend', () => {
+        cards[slide].classList.remove('w3-animate-opacity');
+        cards[slide].classList.remove('w3-animate-right');
+        cards[slide].classList.remove('w3-animate-left');
+      });
+    }
+    else if (direction == "left") {
+      cards[slide].classList.toggle('w3-animate-right');
+    }
+    else if (direction == "right") {
+      cards[slide].classList.toggle('w3-animate-left');
+    }
+
   }
-
-
-
-
 }
